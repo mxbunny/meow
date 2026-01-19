c116353820a958a12b2459089f2d316fb2f1c1ac
mxbunny <100415196+mxbunny@users.noreply.github.com>
2026-01-19 12:54:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you meow!";
+  question.innerHTML = "Yay, see you meow! DM me and let's go on rift ♥";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
@@ -21,3 +21,4 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
